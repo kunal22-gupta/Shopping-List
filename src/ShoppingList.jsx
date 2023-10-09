b58d628922e02f01be2c83d0d6b391f76ad1dc93
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-10-09 22:04:15 +0530

    feat: display message when item is removed

--- src/ShoppingList.jsx
+++ src/ShoppingList.jsx
@@ -6,14 +6,30 @@ import { useState } from "react";
 
 export default function ShoppingList() {
     const uid = new ShortUniqueId();
+    const [lastRemoveItem, setLastRemoveItem] = useState("Click on items to remove them");
     const [itemsList, setItemsList] = useState([
         { id: uid.rnd(), product: "Apples", qty: 5 },
         { id: uid.rnd(), product: "Bananas", qty: 4 },
     ]);
+
+    
+    console.log(itemsList);
+
+    // Remove an item from the list
     const removeItem = (e) => {
-        e.target.localName === "span"
-            ? e.target.parentNode.remove()
-            : e.target.remove();
+        // Select the li element
+        const li = e.target.localName === "span" ? e.target.parentNode : e.target;
+
+        // Remove item from state
+        setItemsList((currItemsList) => {
+            return currItemsList.filter((item) => {
+                if (item.id === li.id) {
+                    console.log("removed: ", item.product);
+                    setLastRemoveItem(`${item.product} removed!`);
+                }
+                return item.id !== li.id;
+            });
+        });
     };
     return (
         <div className="ShoppingList">
@@ -21,12 +37,14 @@ export default function ShoppingList() {
             <ul>
                 {itemsList.map((item) => {
                     return (
-                        <li key={item.id} onClick={removeItem}>
+                        <li id={item.id} key={item.id} onClick={removeItem}>
                             <span>{item.product}</span> <span>x{item.qty}</span>
                         </li>
                     );
                 })}
             </ul>
+            <hr/>
+            <p className="mini-text">{lastRemoveItem}</p>
             <ShoppingListForm setItemsList={setItemsList} uid={uid} />
         </div>
     );
